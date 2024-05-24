a6869eb88f24242b6f1253d6c2f60eb2df1fee52
petekertemm <petek@cratustech.com>
2024-05-24 15:07:49 +0300

    Header added
    Header added

--- online-grocery/app/(routes)/(site)/page.tsx
+++ online-grocery/app/(routes)/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>asd</div>;
+  return <div>Anasayfa</div>;
 }
---
+++ online-grocery/app/(routes)/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default MainLayout;
